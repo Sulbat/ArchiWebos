c3e4e6bdc6ccecd49bf64ccbab5229ae1483f3d2
Sami Kibachi <samikibachi@gmail.com>
2023-03-13 19:50:15 +0100

    Ajout page login non fonctionelle + filtres fonctionnels

---
+++ FrontEnd/login.js
@@ -0,0 +1,43 @@
+const loginForm = document.getElementById("login-form");
+const emailInput = document.getElementById("email-input");
+const passwordInput = document.getElementById("password-input");
+const loginButton = document.getElementById("loginButton");
+const loginErrorMsg = document.querySelector(".login-error-msg");
+
+  
+loginForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  console.log(email, password);
+
+  loginButton.innerHTML = " ";
+  loginButton.setAttribute("disabled", true);
+
+  try {
+    const response = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    loginButton.innerHTML += "Connexion en cours...";
+    if (!response.ok) {
+      throw new Error("Echec de la connexion");
+    }
+
+    const { userId, token } = await response.json();
+    localStorage.setItem("userId", userId);
+    localStorage.setItem("token", token);
+    console.log(userId, token);
+    window.location.href = "index.html";
+  } catch (error) {
+    console.log(error);
+    loginErrorMsg.style.display = "block";
+    loginButton.innerHTML += "Echec de la connexion";
+    loginButton.removeAttribute("disabled");
+  }
+});
--- FrontEnd/projets.js
+++ FrontEnd/projets.js
@@ -1,39 +1,85 @@
-fetch('http://localhost:5678/api/works')
+fetch("http://localhost:5678/api/works")
+  // On convertit la réponse en JSON
+  .then((response) => response.json())
+  .then((data) => {
+    // On boucle sur les données
+    for (const item of data) {
+      // On crée une nouvelle figure
+      const figure = document.createElement("figure");
+
+      // On crée la balise image
+      const img = document.createElement("img");
+      img.src = item.imageUrl;
+      img.alt = item.title;
+      img.names = item.category.name;
+      console.log(img.names);
+
+      // On crée la balise légende
+      const figcaption = document.createElement("figcaption");
+      figcaption.textContent = item.title;
+
+      // On ajoute l'image et la légende à la figure
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
+
+      // On ajoute la figure au conteneur "gallery"
+      document.querySelector(".gallery").appendChild(figure);
+    }
 
 
-// On convertit la réponse en JSON
-.then(response => response.json())
-.then(data => {
+    //Filtres
+    const allFilter = document.querySelector(".tous");
+    const objectFilter = document.querySelector(".objets");
+    const apartmentFilter = document.querySelector(".appartements");
+    const hotelRestaurantFilter = document.querySelector(".hotel");
 
 
-  // On boucle sur les données
-  for (const item of data) {
+    //Tous (avec fonction anonyme) 
+    allFilter.addEventListener("click", function () {
+      const figures = document.querySelectorAll("figure");
+      for (const figure of figures) {
+        figure.style.display = "block";
+      }
+    });
 
 
-  // On crée une nouvelle figure
-    const figure = document.createElement('figure');
-    
-    // On crée la balise image
-    const img = document.createElement('img');
-    img.src = item.imageUrl;
-    img.alt = item.title;
-    img.names = item.category.name;
-    console.log(img.names);
-    
-    
-    // On crée la balise légende
-    const figcaption = document.createElement('figcaption');
-    figcaption.textContent = item.title;
+    //Objets
+    objectFilter.addEventListener("click", function () {
+      const figures = document.querySelectorAll("figure");
+      for (const figure of figures) {
+        if (figure.querySelector("img").names === "Objets") {
+          figure.style.display = "block";
+        } else {
+          figure.style.display = "none";
+        }
+      }
+    });
 
-    
-    // On ajoute l'image et la légende à la figure
-    figure.appendChild(img);
-    figure.appendChild(figcaption);
-    
-    
-    // On ajoute la figure au conteneur "gallery"
-    document.querySelector('.gallery').appendChild(figure);
-  }
+
+    // Appartements 
+    apartmentFilter.addEventListener("click", function () {
+      const figures = document.querySelectorAll("figure");
+      for (const figure of figures) {
+        if (figure.querySelector("img").names === "Appartements") {
+          figure.style.display = "block";
+        } else {
+          figure.style.display = "none";
+        }
+      }
+    });
+
+
+    // Hotels & Restaurants
+    hotelRestaurantFilter.addEventListener('click', function() {
+      const figures = document.querySelectorAll('figure');
+      for (const figure of figures) {
+        if (figure.querySelector('img').names === 'Hotels & restaurants') {
+          figure.style.display = 'block';
+        } else {
+          figure.style.display = 'none';
+        }
+      }
+    });
+
   
-});
-
+  });
