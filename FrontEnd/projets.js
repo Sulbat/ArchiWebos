3edbc797321fe5e8f9c27d28181c510f9b7400cd
Nicolas vauché <hello@nicolasvauche.net>
2023-03-14 15:29:00 +0100

    Opti

--- FrontEnd/projets.js
+++ FrontEnd/projets.js
@@ -1,30 +1,9 @@
-fetch("http://localhost:5678/api/works")
+window.addEventListener('DOMContentLoaded', () => {
+  fetch("http://localhost:5678/api/works")
   // On convertit la réponse en JSON
   .then((response) => response.json())
   .then((data) => {
-    // On boucle sur les données
-    for (const item of data) {
-      // On crée une nouvelle figure
-      const figure = document.createElement("figure");
-
-      // On crée la balise image
-      const img = document.createElement("img");
-      img.src = item.imageUrl;
-      img.alt = item.title;
-      img.names = item.category.name;
-      console.log(img.names);
-
-      // On crée la balise légende
-      const figcaption = document.createElement("figcaption");
-      figcaption.textContent = item.title;
-
-      // On ajoute l'image et la légende à la figure
-      figure.appendChild(img);
-      figure.appendChild(figcaption);
-
-      // On ajoute la figure au conteneur "gallery"
-      document.querySelector(".gallery").appendChild(figure);
-    }
+    show(data)
 
 
     //Filtres
@@ -83,3 +62,31 @@ fetch("http://localhost:5678/api/works")
 
   
   });
+
+  /** Add projetcs to the DOM */
+  const show = data => {
+    // On boucle sur les données
+    for (const item of data) {
+      // On crée une nouvelle figure
+      const figure = document.createElement("figure");
+
+      // On crée la balise image
+      const img = document.createElement("img");
+      img.src = item.imageUrl;
+      img.alt = item.title;
+      img.names = item.category.name;
+      console.log(img.names);
+
+      // On crée la balise légende
+      const figcaption = document.createElement("figcaption");
+      figcaption.textContent = item.title;
+
+      // On ajoute l'image et la légende à la figure
+      figure.appendChild(img);
+      figure.appendChild(figcaption);
+
+      // On ajoute la figure au conteneur "gallery"
+      document.querySelector(".gallery").appendChild(figure);
+    }
+  }
+})
